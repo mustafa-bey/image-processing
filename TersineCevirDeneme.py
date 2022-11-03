8abd0626554ae37299bcbf26f747dfeb732b7737
mustafa <110256602+mustafa-bey@users.noreply.github.com>
2022-11-03 22:48:59 +0300

    Update TersineCevirDeneme.py

--- TersineCevirDeneme.py
+++ TersineCevirDeneme.py
@@ -9,36 +9,29 @@ im_invert.save('data/dst/lena_invert.jpg', quality=95)
 
 """
 
-normal = cv2.imread("resim.jpg")                           #resim 400x400 oldugu için piksel oranı belirli
+normal = cv2.imread("resim.jpg",0)                           #resim 400x400 oldugu için piksel oranı belirli
 cv2.imshow("el",normal)
 
-print(normal)
+newM = [k[:] for k in normal]
 
+dup = []
+for k in newM:
+    for i in k:
+        dup.append(i)
+#print (max(dup), min(dup))
+a = max(dup)
 
-matris = [],[]
-for i in range(400):                                      # görüntü tüm elemanlarını bir dizide tutmak için kullan
+#print(newM)
+
+for i in range(400):
     for j in range(400):
-        matris[i][j]=normal
+        newM[i][j]= int(newM[i][j]) - a     #burada bir sıkıntı var int() yazmayınca hata alıyorum bu koda bak
 
 
-a = max(matris)                                           #matrisin en büyük degerini bir değiskene atar
-
-for i in range(400):                                      # burada göruntuyu tersine çevirme işleminı gerçekleştirdik
-    for j in range(400):
-        matris[i][j]=matris[i][j] - a
-
-
-invert = matris                                           #burada matrisi resim olarak okumaya calıstık
-cv2.imshow("ters_gorontu",invert)
-
-
-#gray = cv2.imread("resim.jpg",0)                         #burası gereksiz kullanmasan da olur
-#cv2.imshow("gri_el",gray)
-
-
-deneme =[2,3,4,5],[5,6,89,9],[1,1,2,4],[65,234,45,77]
-cv2.imshow("deneme",deneme)
+plt.plot(newM)
+plt.show()
 
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
